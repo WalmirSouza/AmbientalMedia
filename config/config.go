ce920f5695b1c04ac3a658a79cf10d7e3a9809ed
Walmir Oliveira de Souza <walmir.souza@thomasgregit.com.br>
2023-12-16 01:52:32 -0300

    Criado Banco de Dados: SQLite

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 var (
 	db     *gorm.DB
@@ -8,8 +12,19 @@ var (
 )
 
 func Initialize() error {
+	var err error
+
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("Erro ao Inicializar SQLite: %v", err)
+	}
 
 	return nil
+}
+
+func GetSQLiteDatabase() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
---
+++ config/sqlite.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"os"
+
+	"github.com/WalmirSouza/Challenge/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+
+	dbPath := "./db/main.db"
+
+	_, err := os.Stat((dbPath))
+
+	if os.IsNotExist(err) {
+		logger.Info("Criando banco de dados...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create((dbPath))
+
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Error("Erro ao abrir o Banco de dados: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate((&schemas.WindForecast{}))
+
+	if err != nil {
+		logger.Errorf("Erro no Automigrate Sqlite: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
